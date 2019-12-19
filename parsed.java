20c31af660b0ec054825692bdb001ef2c4ef2b39
Jake Motta <jake.motta@klearexpress.com>
2019-12-19 09:59:31 -0800

    Fixed formatting to adjust for email clients destorying flexbox

---
+++ parsed.java
@@ -0,0 +1,242 @@
+this.htmlBody = "<!-- /*************************************************************************\n" +
+                " *\n" +
+                " * KLEARNOW CONFIDENTIAL\n" +
+                " * __________________\n" +
+                " *\n" +
+                " *  Copyright (c) 2018 - 2018 KLEARNOW Corporation.\n" +
+                " *  All Rights Reserved.\n" +
+                " *\n" +
+                " * NOTICE:  All information contained herein is, and remains\n" +
+                " * the property of KLEARNOW Corporation and its suppliers,\n" +
+                " * if any.  The intellectual and technical concepts contained\n" +
+                " * herein are proprietary to KLEARNOW Corporation\n" +
+                " * and its suppliers and may be covered by U.S. and Foreign Patents,\n" +
+                " * patents in process, and are protected by trade secret or copyright law.\n" +
+                " * Dissemination of this information or reproduction of this material\n" +
+                " * is strictly forbidden unless prior written permission is obtained\n" +
+                " * from KLEARNOW Corporation.\n" +
+                " */ -->\n" +
+                "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "  <head>\n" +
+                "    <script type=\"text/javascript\">\n" +
+                "      function myFunction(url) {\n" +
+                "        window.open(url, \"_blank\")\n" +
+                "      }\n" +
+                "    </script>\n" +
+                "    <title>KlearNow</title>\n" +
+                "    <style>\n" +
+                "      .wrapper {\n" +
+                "        width: 80%;\n" +
+                "        max-width: 960px;\n" +
+                "        min-height: 800px;\n" +
+                "        margin: auto;\n" +
+                "        margin-top: 25px;\n" +
+                "        background-color: white;\n" +
+                "        padding: 10px;\n" +
+                "        border-radius: 4px;\n" +
+                "      }\n" +
+                "      .header {\n" +
+                "        float: left;\n" +
+                "        padding: 15px 0px;\n" +
+                "        width: 100%;\n" +
+                "      }\n" +
+                "      .content-wrapper {\n" +
+                "        float: left;\n" +
+                "        width: 100%;\n" +
+                "        box-shadow: 0 2px 4px 0 rgba(146, 146, 146, 0.5);\n" +
+                "      }\n" +
+                "      .banner {\n" +
+                "        height: 140px;\n" +
+                "        border-radius: 2px 2px 0 0;\n" +
+                "        background-color: #7adecb;\n" +
+                "        background: linear-gradient(207.07deg, #04909e 0%, #3db6b4 37.57%, #7adecb 100%);\n" +
+                "      }\n" +
+                "      .banner-section {\n" +
+                "        width: 48%;\n" +
+                "        height: 100%;\n" +
+                "        float: left;\n" +
+                "        text-align: center;\n" +
+                "        padding: 0px 1%;\n" +
+                "      }\n" +
+                "      .banner-text-filler {\n" +
+                "        width: 100%;\n" +
+                "        height: 18px;\n" +
+                "      }\n" +
+                "      .invis {\n" +
+                "        color: transparent;\n" +
+                "      }\n" +
+                "      .banner-text {\n" +
+                "        color: white;\n" +
+                "        text-align: right;\n" +
+                "        font-size: 16px;\n" +
+                "        margin: auto;\n" +
+                "        padding: 65px 10px 0px 0px;\n" +
+                "      }\n" +
+                "      .content {\n" +
+                "        float: left;\n" +
+                "        padding: 5px 10%;\n" +
+                "        text-align: center;\n" +
+                "        font-size: 20px;\n" +
+                "        font-weight: 100;\n" +
+                "        color: #090b0b;\n" +
+                "        background-color: #fafafa;\n" +
+                "      }\n" +
+                "      .register-button {\n" +
+                "        width: 70%\n"+
+                "        max-width: 290px;\n" +
+                "        border-radius: 3px;\n" +
+                "        border: none;\n" +
+                "        background-color: #7adecb;\n" +
+                "        background: linear-gradient(207.07deg, #04909e 0%, #3db6b4 37.57%, #7adecb 100%);\n" +
+                "        cursor: pointer;\n" +
+                "        transition: 0.2s ease;\n" +
+                "        color: white;\n" +
+                "        font-size: 16px;\n" +
+                "        margin: auto;\n" +
+                "        padding: 15px 0px;\n" +
+                "      }\n" +
+                "      .register-button:hover {\n" +
+                "        box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.37);\n" +
+                "      }\n" +
+                "      button:focus {\n" +
+                "        outline: 0;\n" +
+                "      }\n" +
+                "      .title {\n" +
+                "        font-size: 30px;\n" +
+                "        font-weight: bold;\n" +
+                "      }\n" +
+                "      .username {\n" +
+                "        font-weight: 400;\n" +
+                "      }\n" +
+                "      .content-link:link {\n" +
+                "        color: #28c4ff;\n" +
+                "        transition: 0.2s ease;\n" +
+                "        text-decoration: none;\n" +
+                "        font-weight: 400;\n" +
+                "      }\n" +
+                "      .content-link:hover {\n" +
+                "        color: #04909e;\n" +
+                "      }\n" +
+                "      .content-link:visited {\n" +
+                "        color: #28c4ff;\n" +
+                "      }\n" +
+                "      .footer {\n" +
+                "        float: left;\n" +
+                "        width: 100%;\n" +
+                "        font-size: 16px;\n" +
+                "        color: #5b5858;\n" +
+                "        margin-top: 25px;\n" +
+                "        line-height: 1.5;\n" +
+                "      }\n" +
+                "      .image {\n" +
+                "        margin: 0;\n" +
+                "        border: 0;\n" +
+                "        padding: 0;\n" +
+                "        display: block;\n" +
+                "      }\n" +
+                "      .banner-left,\n" +
+                "      .banner-right {\n" +
+                "         display: block;\n" +
+                "      }\n" +
+                "      .body-text {\n" +
+                "         text-align: left;\n" +
+                "      }\n" +
+                "      .computer {\n" +
+                "         padding: 10px 0px 0px 10px;\n" +
+                "      }\n" +
+                "      @media only screen and (max-width: 800px) {\n" +
+                "        .wrapper {\n" +
+                "          width: 80%;\n" +
+                "        }\n" +
+                "        .banner-text-filler-right {\n" +
+                "          height: 50%;\n" +
+                "        }\n" +
+                "        .title {\n" +
+                "          font-size: 24px;\n" +
+                "        }\n" +
+                "        .banner-section {\n" +
+                "          width: 48%;\n" +
+                "          padding: 0px 1%;\n" +
+                "        }\n" +
+                "      }\n" +
+                "      @media only screen and (max-width: 500px) {\n" +
+                "        .banner-left,\n" +
+                "        .banner-text-filler,\n" +
+                "        .invis {\n" +
+                "          display: none;\n" +
+                "        }\n" +
+                "        .title {\n" +
+                "          font-size: 22px;\n" +
+                "        }\n" +
+                "        .banner {\n" +
+                "          height: 58px;\n" +
+                "        }\n" +
+                "        .banner-section {\n" +
+                "          width: 98%;\n" +
+                "          padding: 0px 2%;\n" +
+                "        }\n" +
+                "        .banner-text {\n" +
+                "          padding: 20px 0px;\n" +
+                "          text-align: center;\n" +
+                "        }\n" +
+                "        .content {\n" +
+                "          padding: 5px 10px;\n" +
+                "        }\n" +
+                "        .register-button {\n" +
+                "          width: 70%;\n" +
+                "        }\n" +
+                "        .header {\n" +
+                "          width: 100%;\n" +
+                "        }\n" +
+                "        .topLogo {\n" +
+                "          margin: auto;\n" +
+                "        }\n" +
+                "        p {\n" +
+                "          font-size: 16px;\n" +
+                "        }\n" +
+                "        .body-text {\n" +
+                "          text-align: center;\n" +
+                "        }\n" +
+                "      }\n" +
+                "    </style>\n" +
+                "  </head>\n" +
+                "  <body>\n" +
+                "    <div class=\"wrapper\">\n" +
+                "      <div class=\"header\">\n" +
+                "        <img class=\"image topLogo\" width=\"200\" height=\"24.34\" src=\"https://raw.githubusercontent.com/jacobkx/kn-images/master/kn_logo.png\" />\n" +
+                "      </div>\n" +
+                "      <div class=\"content-wrapper\">\n" +
+                "        <div class=\"banner\">\n" +
+                "          <div class=\"banner-section banner-left\">\n" +
+                "          <img class=\"computer image\" width=\"180\" height=\"122\" src=\"https://raw.githubusercontent.com/jacobkx/kn-images/master/computer.png\" />\n" +
+                "        </div>\n" +
+                "        <div class=\"banner-section banner-right\">\n" +
+                "          <div class=\"banner-text\">CUSTOMS CLEARANCE SIMPLIFIED</div>\n" +
+                "        </div>\n" +
+                "      </div>\n" +
+                "      <div class=\"content\">\n" +
+                "        <p class=\"title\">FIRST_LINE</p>\n" +
+                "        <p class=\"body-text\">\n" +
+                "          MESSAGE\n" +
+                "        </p>\n" +
+                "        <a class=\"content-link\" href=\"URL\">\n" +
+                "          <div class=\"register-button\">COMPLETE REGISTRATION</div>\n" +
+                "        </a>\n" +
+                "        <p>\n" +
+                "          If above link does not work, please <a class=\"content-link\" href=\"URL\">click here!</a> <br />\n" +
+                "          For any questions and help, please <a class=\"content-link\" href=\"mailto:info@klearnow.com\">contact us.</a>\n" +
+                "        </p>\n" +
+                "      </div>\n" +
+                "      </div>\n" +
+                "      <div class=\"footer\">\n" +
+                "        <div><img width=\"100\" height=\"12.17\" class=\"image\" style=\"display: inline\" src=\"https://raw.githubusercontent.com/jacobkx/kn-images/master/kn_logo.png\" /> CONFIDENTIAL</div>\n" +
+                "        <div>Copyright (c) 2018 - 2019 KlearNow Corporation. All Rights Reserved.</div>\n" +
+                "        <p>\n" +
+                "          NOTICE: All information contained herein is, and remains the property of KlearNow Corporation and its suppliers, if any. The intellectual and technical concepts contained herein are proprietary to KlearNow Corporation and its suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained\n" +
+                "          from KlearNow Corporation.\n" +
+                "        </p>\n" +
+                "      </div>\n" +
+                "    </div>\n" +
+                "  </body>\n" +
+                "</html>";
